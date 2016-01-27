2162ed34389619c6d21ffae108e8c8e2e1c661e1
Luca Boccassi <lboccass@brocade.com>
2016-01-27 14:13:57 +0100

    Problem: no cmdline option for number of sends
    Solution: add -m cmdline flag, use it to send m messages and exit

--- src/sender.c
+++ src/sender.c
@@ -2,12 +2,14 @@
 #include <getopt.h>
 #include <malamute.h>
 
+unsigned int num_messages;
 unsigned int interval = 1000;
 char *endpoint;
 char *name;
 
 static struct option longopts[] = {
     { "endpoint",    required_argument,       NULL, 'e' },
+    { "num_messages",  required_argument,       NULL, 'm' },
     { "interval", required_argument,       NULL, 'i' },
     { "name", required_argument,       NULL, 'n' },
     { NULL ,    0,         NULL, '\0' }
@@ -18,7 +20,8 @@ static void usage(void)
     fprintf(stderr,
         "Usage: %s -e | --endpoint tcp://1.2.3.4:5\n"
         "       -n | --name name of this instance \n"
-        "       [[-i | --interval] sleep between sends (milliseconds), default 1000]\n",
+        "       [[-i | --interval] sleep between sends (milliseconds), default 1000]\n"
+        "       [[-m | --num_messages] number of messages to send, default unlimited]\n",
         "sender");
     exit(1);
 }
@@ -27,7 +30,7 @@ static void parse_args(int argc, char **argv)
 {
     int opt;
 
-    while ((opt = getopt_long(argc, argv, "i:e:n:",
+    while ((opt = getopt_long(argc, argv, "i:e:n:m:",
                   longopts, 0)) != -1) {
         switch (opt) {
         case 'i':
@@ -38,6 +41,9 @@ static void parse_args(int argc, char **argv)
             break;
         case 'n':
             name = strdup(optarg);
+            break;
+        case 'm':
+            num_messages = atoi(optarg);
             break;
 
         default:
@@ -74,7 +80,7 @@ int main (int argc, char **argv) {
         return -2;
     }
 
-    while (!zsys_interrupted) {
+    while (!zsys_interrupted && (!num_messages || count < num_messages)) {
         zclock_sleep(interval);
         zmsg_t *msg = zmsg_new();
         assert (msg);
