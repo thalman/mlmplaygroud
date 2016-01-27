f2efa43ab552277b996943647d46fe15eab35980
Luca Boccassi <lboccass@brocade.com>
2016-01-27 14:12:33 +0100

    Problem: no cmdline option for send interval
    Solution: add -i cmdline flag, use it to sleep between sends

--- src/sender.c
+++ src/sender.c
@@ -2,11 +2,13 @@
 #include <getopt.h>
 #include <malamute.h>
 
+unsigned int interval = 1000;
 char *endpoint;
 char *name;
 
 static struct option longopts[] = {
     { "endpoint",    required_argument,       NULL, 'e' },
+    { "interval", required_argument,       NULL, 'i' },
     { "name", required_argument,       NULL, 'n' },
     { NULL ,    0,         NULL, '\0' }
 };
@@ -15,7 +17,8 @@ static void usage(void)
 {
     fprintf(stderr,
         "Usage: %s -e | --endpoint tcp://1.2.3.4:5\n"
-        "       -n | --name name of this instance \n",
+        "       -n | --name name of this instance \n"
+        "       [[-i | --interval] sleep between sends (milliseconds), default 1000]\n",
         "sender");
     exit(1);
 }
@@ -24,9 +27,12 @@ static void parse_args(int argc, char **argv)
 {
     int opt;
 
-    while ((opt = getopt_long(argc, argv, "e:n:",
+    while ((opt = getopt_long(argc, argv, "i:e:n:",
                   longopts, 0)) != -1) {
         switch (opt) {
+        case 'i':
+            interval = atoi(optarg);
+            break;
         case 'e':
             endpoint = strdup(optarg);
             break;
@@ -69,7 +75,7 @@ int main (int argc, char **argv) {
     }
 
     while (!zsys_interrupted) {
-        zclock_sleep(1000);
+        zclock_sleep(interval);
         zmsg_t *msg = zmsg_new();
         assert (msg);
         zmsg_pushstr (msg, "hello");
